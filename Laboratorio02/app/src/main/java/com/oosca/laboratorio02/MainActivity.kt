31f8d6a861745339042ab65590134024baba445e
Omi1212 <000666621@uca.edu.sv>
2023-03-24 22:28:01 -0600

    Laboratorio 02

--- Laboratorio02/app/src/main/java/com/oosca/laboratorio02/MainActivity.kt
+++ Laboratorio02/app/src/main/java/com/oosca/laboratorio02/MainActivity.kt
@@ -2,10 +2,87 @@ package com.oosca.laboratorio02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var weightEditText: EditText
+    private lateinit var heightEditText: EditText
+    private lateinit var actionCalculate: Button
+    private lateinit var bmiTextView: TextView
+    private lateinit var resultTextView: TextView
+    private lateinit var infoTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bind()
+        setListener()
     }
+
+    private fun bind(){
+        weightEditText = findViewById(R.id.weight_edit_text)
+        heightEditText = findViewById(R.id.height_edit_text)
+        actionCalculate = findViewById(R.id.action_calculate)
+        bmiTextView = findViewById(R.id.bmi_text_view)
+        resultTextView = findViewById(R.id.result_text_view)
+        infoTextView = findViewById(R.id.info_text_view)
+    }
+
+    private fun setListener(){
+        actionCalculate.setOnClickListener{
+            val weight = weightEditText.text.toString()
+            val height = heightEditText.text.toString()
+
+
+
+            val bmi = bmi_cal(height.toFloat(),weight.toFloat())
+            val bmiTwoDigits = String.format("%.2f", bmi)
+
+
+            result(bmiTwoDigits.toFloat())
+        }
+    }
+
+    private fun bmi_cal(height: Float, weight: Float): Float{
+        return weight / ((height/100) * (height/100))
+    }
+
+    private fun result(bmi: Float){
+        bmiTextView.text = bmi.toString()
+        infoTextView.text = "(Normal range is 18.5 - 24.9)"
+        var informationResult = ""
+        var color = 0
+
+        when{
+            bmi < 18.50 -> {
+                informationResult = "Underweight"
+                color = R.color.under_weight
+            }
+            bmi in 18.50..24.99 -> {
+                informationResult = "Healthy"
+                color = R.color.normal_weight
+            }
+            bmi in 25.00..29.99 -> {
+                informationResult = "Overweight"
+                color = R.color.over_weight
+            }
+            bmi > 18.50 -> {
+                informationResult = "Obese"
+                color = R.color.obese
+            }
+        }
+
+        resultTextView.text = informationResult
+        resultTextView.setTextColor(ContextCompat.getColor(this, color))
+    }
+
+
+
+
 }
