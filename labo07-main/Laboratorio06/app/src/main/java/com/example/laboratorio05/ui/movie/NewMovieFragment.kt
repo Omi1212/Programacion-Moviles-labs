ba364c5cf8305100844888ebdac80493c0e2ea85
Omi1212 <000666621@uca.edu.sv>
2023-05-05 10:35:41 -0600

    add Laboratorio07

---
+++ labo07-main/Laboratorio06/app/src/main/java/com/example/laboratorio05/ui/movie/NewMovieFragment.kt
@@ -0,0 +1,73 @@
+package com.example.laboratorio05.ui.movie
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import androidx.navigation.fragment.findNavController
+import com.example.laboratorio05.MovieReviewerApplication
+
+import com.example.laboratorio05.R
+import com.example.laboratorio05.data.model.MovieModel
+import com.example.laboratorio05.databinding.FragmentNewMovieBinding
+
+class NewMovieFragment : Fragment() {
+
+    private val movieViewModel: MovieViewModel by activityViewModels {
+        MovieViewModel.Factory
+    }
+    lateinit var binding: FragmentNewMovieBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentNewMovieBinding.inflate(inflater,container,false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setViewModel()
+        observeStatus()
+    }
+
+    private fun setViewModel(){
+        binding.viewmodel = movieViewModel
+    }
+
+    private fun observeStatus(){
+        movieViewModel.status.observe(viewLifecycleOwner) { status ->
+            when{
+                status.equals(MovieViewModel.MOVIE_CREATED) -> {
+                    Log.d(APP_TAG, status)
+                    Log.d(APP_TAG, movieViewModel.getMovies().toString())
+                    movieViewModel.clearData()
+                    movieViewModel.clearStatus()
+                    findNavController().popBackStack()
+                }
+                status.equals(MovieViewModel.WRONG_INFORMATION) -> {
+                    Log.d(APP_TAG, status)
+                    movieViewModel.clearStatus()
+
+                }
+            }
+        }
+    }
+    companion object{
+
+        const val APP_TAG = "APP_TAG"
+    }
+
+}
+
